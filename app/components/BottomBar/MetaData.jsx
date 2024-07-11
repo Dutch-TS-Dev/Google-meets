d276c7db243e405d4aa76ddcaec6bfcad375a921
oscar <oscar.vanvelsen@gmail.com>
2024-07-11 12:11:47 +0200

    finish metadata

---
+++ app/components/BottomBar/MetaData.jsx
@@ -0,0 +1,8 @@
+export default function MetaData() {
+  return (
+    <div className="text-white">
+      <span className="pr-2"> 11:59 AM </span>
+      <span className="pl-3 seperator">|</span> roj-yhcr-gxd
+    </div>
+  );
+}
