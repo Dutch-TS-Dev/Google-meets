253c125384dc4ab1a44abd6a4ff68f87e7026704
oscar <oscar.vanvelsen@gmail.com>
2024-07-11 12:28:57 +0200

    add icons fix metadata

--- app/components/BottomBar/ButtonsRight.jsx
+++ app/components/BottomBar/ButtonsRight.jsx
@@ -1,1 +1,11 @@
-// Oscar
+export default function ButtonsRight() {
+  return (
+    <div className="images flex [&>img]:w-5 gap-4">
+      <img src="/activities.png" />
+      <img src="/chat.png" />
+      <img src="/groupe.png" />
+      <img src="/info.png" />
+      <img src="/lock.png" />
+    </div>
+  );
+}
