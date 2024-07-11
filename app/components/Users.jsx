f6c1b0b6537d12ab8ed210bbb66bea901046d097
PoSchB <kabn@web.de>
2024-07-11 12:26:55 +0200

    usersAdd

--- app/components/Users.jsx
+++ app/components/Users.jsx
@@ -1,4 +1,21 @@
 //   Paul
+import users from "../users.json"
+import ".//users.css"
+
 export default function Users() {
-  return <>Users comp</>;
+
+
+    return (
+        <>
+            <div className="profileCard-wrapper-1">
+                {users.map((user) => (
+                    <div key={user.id} className="profileCard">
+                        {/* <UserIcon /> */}
+                        <img src={user.avatar} alt={user.name} />
+                        <h4>{user.name}</h4>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
